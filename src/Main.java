480ab08743c2918c78c6345eee17cba8a1b8356b
sahilk-create <sahilkesharwani2512@gmail.com>
2026-01-09 10:14:44 +0530

    Implement reading and summing two integers

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
 
-        // TODO: Read two integers
-      
-
-        // TODO: Print their sum  using println
-      
+        System.out.println(num1 + num2);
+        
         scanner.close();
     }
 }
